797abff753f950bd16748b0b7de77a01184ed341
L.A.A <mr.leoalves@gmail.com>
2023-10-12 08:29:55 -0300

    Carregando a lista de forma dinâmica

--- script/main.js
+++ script/main.js
@@ -2,10 +2,39 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset${offset}0&limits${limit}`;
 
+function convertPokemonHtml(pokemon){
+    return `
+    <li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name">${pokemon.name}</span>
+    
+    <div class="details">
+      <ol class="types">
+        <li class="type">Grass</li>
+        <li class="type">Poison</li>
+      </ol>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}" >
+    </div>
+  </li>
+    
+  `
+}
+
+const pokemonList = document.getElementById('pokemonList');
 
 fetch(url)
-    .then((response)=>response.json()
-    .then((responseBody)=>response.json())
-    .then((jsonBody)=> console.log(jsonBody))
-).catch((error)=>console.log(error))
-.finally(()=>console.log("Request completed"));
+    .then((response)=>response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) =>{
+        
+        for(let i = 0; i < pokemons.length;i++){
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonHtml(pokemon);
+           // document.getElementsByClassName('pokemons')
+        }
+        /*
+        debugger
+        console.log(pokemonList)
+        */
+    })
+    .catch((error)=>console.log(error))
